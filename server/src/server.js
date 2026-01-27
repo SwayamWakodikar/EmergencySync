9cb7bd5ffe9b2f760e52dc1ce98441c66d6cb156
SwayamWakodikar <swayam.w06@gmail.com>
2026-01-27 08:32:34 +0530

    feat(server): initialize backend with Express, CORS, and dotenv setup

---
+++ server/src/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+const app= express();
+const port=process.env.PORT;
+app.use(cors());
+
+app.get("/",(req,res)=>{
+    console.log("got poked")
+}
+)
+
+app.listen(port,()=>{
+    console.log(`Server Running at port ${port}`);
+})
