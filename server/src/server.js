021b3066a36f6370f136883c71d71aa1512542bf
SwayamWakodikar <swayam.w06@gmail.com>
2026-01-28 17:04:29 +0530

    code formatting

--- server/src/server.js
+++ server/src/server.js
@@ -2,15 +2,14 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
-const app= express();
-const port=process.env.PORT;
+const app = express();
+const port = process.env.PORT;
 app.use(cors());
-app.use(express.json())
-app.get("/",(req,res)=>{
-    console.log("got poked")
-}
-)
+app.use(express.json());
+app.get("/", (req, res) => {
+  console.log("got poked");
+});
 
-app.listen(port,()=>{
-    console.log(`Server Running at port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server Running at port ${port}`);
+});
