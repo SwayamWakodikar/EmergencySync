36bc80d4eb8b25b65346e55ed2839bd98ace8771
SwayamWakodikar <swayam.w06@gmail.com>
2026-02-09 21:06:15 +0530

    fix: prisma.js DATABASE_URL env extraction

--- server/src/prisma.js
+++ server/src/prisma.js
@@ -1,9 +1,15 @@
-import 'dotenv/config'
+// prisma.js
+import dotenv from 'dotenv'
+dotenv.config()
 import { PrismaClient } from '@prisma/client'
 import { withAccelerate } from '@prisma/extension-accelerate'
+
+// if (!process.env.DATABASE_URL) {
+//   throw new Error('DATABASE_URL is not set')
+// }
 
 const prisma = new PrismaClient({
   accelerateUrl: process.env.DATABASE_URL,
 }).$extends(withAccelerate())
 
-export default prisma;
+export default prisma
--- server/src/seed.js
+++ server/src/seed.js
@@ -1,44 +1,32 @@
-// @ts-nocheck
 import 'dotenv/config'
-import { PrismaClient } from '@prisma/client'
-import { withAccelerate } from '@prisma/extension-accelerate'
+import prisma from './prisma.js'
 
-const prisma = new PrismaClient({
-  accelerateUrl: process.env.DATABASE_URL,
-}).$extends(withAccelerate())
-
-
-//setting bounds for a particular city Eg:- Pune
-const LAT_MIN = 18.45;
-const LAT_MAX = 18.65;
-const LNG_MIN = 73.75;
-const LNG_MAX = 73.95;
+const LAT_MIN = 18.45
+const LAT_MAX = 18.65
+const LNG_MIN = 73.75
+const LNG_MAX = 73.95
 
 function randomBetween(min, max) {
-  return Math.random() * (max - min) + min;
+  return Math.random() * (max - min) + min
 }
 
 async function seedAmbulances() {
-  await prisma.ambulance.deleteMany();
-
-  //pre-defined Ambulances
-  //let there be n ambulances
-  const n = 10;
+  await prisma.ambulance.deleteMany()
+  const n = 10
   for (let i = 0; i < n; i++) {
     await prisma.ambulance.create({
       data: {
         lat: randomBetween(LAT_MIN, LAT_MAX),
         lng: randomBetween(LNG_MIN, LNG_MAX),
-        status:"FREE"
+        status: 'FREE',
       },
-    });
+    })
   }
   console.log(`added ${n} Ambulances correctly`)
 }
+
 seedAmbulances()
-.catch((err)=>{
-    console.error(err)
-})
-.finally(async()=>{
-    await prisma.$disconnect();
-});
+  .catch(console.error)
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
